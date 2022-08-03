6661ada0e7b7d295a987b8964b70580c51a45fbb
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-08-03 14:15:53 -0500

    Add namespace to every class

--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -1,48 +1,53 @@
 using Godot;
 using System;
 
-public class GameManager : Node2D
+namespace FlappyBird
 {
-    [Export]
-    private float speed = 1f;
-
-    public float Speed { get => speed; }
-
-    public int Points { get; private set; } 
-    public Bird Bird {get; private set; }
-
-    public static GameManager Instance { 
-        get {
-            return istance;
-        } 
-    }
-    private static GameManager istance;
-
-    const string birdPath = "Environment/Bird";
-    const string groundMoverPath = "Environment/Ground/GroundMover";
-    const string levelBuilderPath = "Environment/LevelBuilder";
-
-
-    public void AddPoint() 
+    public class GameManager : Node2D
     {
-        Points += 1;
-    }
+        [Export]
+        private float speed = 1f;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        OS.WindowMaximized = false;
-        istance = this;
-        
-        Points = 0;
-        Bird = GetNode<Bird>(birdPath);
-        GetNode<GroundMover>(groundMoverPath).Speed *= speed;
-        GetNode<GroundMover>(groundMoverPath).Speed *= speed;
-        GetNode<LevelBuilder>(levelBuilderPath).ObstaclesDelay /= speed;
-    }
+        public float Speed { get => speed; }
 
-    public void ResetGame()
-    {
-        GetTree().ReloadCurrentScene();
+        public int Points { get; private set; }
+        public Bird Bird { get; private set; }
+
+        public static GameManager Instance
+        {
+            get
+            {
+                return istance;
+            }
+        }
+        private static GameManager istance;
+
+        const string birdPath = "Environment/Bird";
+        const string groundMoverPath = "Environment/Ground/GroundMover";
+        const string levelBuilderPath = "Environment/LevelBuilder";
+
+
+        public void AddPoint()
+        {
+            Points += 1;
+        }
+
+        // Called when the node enters the scene tree for the first time.
+        public override void _Ready()
+        {
+            OS.WindowMaximized = false;
+            istance = this;
+
+            Points = 0;
+            Bird = GetNode<Bird>(birdPath);
+            GetNode<GroundMover>(groundMoverPath).Speed *= speed;
+            GetNode<GroundMover>(groundMoverPath).Speed *= speed;
+            GetNode<LevelBuilder>(levelBuilderPath).ObstaclesDelay /= speed;
+        }
+
+        public void ResetGame()
+        {
+            GetTree().ReloadCurrentScene();
+        }
     }
 }
--- Scripts/LevelBuilder.cs
+++ Scripts/LevelBuilder.cs
@@ -1,72 +1,75 @@
 using Godot;
 using System;
 
-public class LevelBuilder : Node2D
+namespace FlappyBird
 {
-    public float ObstaclesDelay { get => obstaclesDelay; set {obstaclesDelay = value; GetNode<Timer>("Timer").WaitTime = obstaclesDelay; } }
-
-    [Export]
-    private float obstaclesDelay = 4f;
-    [Export]
-    private int minObstacleHeight = 4;
-    [Export]
-    private int maxObstacleHeight = 9;
-
-    [Export]
-    private float spaceForPlayer = 100f;
-
-    private Pool obstaclesPool;
-    private Pool pointsPool;
-    private Pool obstaclesHeadPool;
-
-    private Vector2 upPosition;
-    private float xBoundary;
-
-    private float height;
-    private Random rnd;
-
-    public override void _Ready()
+    public class LevelBuilder : Node2D
     {
-        obstaclesPool = GetNode<Pool>("ObstaclesPool");
-        pointsPool = GetNode<Pool>("PointsPool");
-        obstaclesHeadPool = GetNode<Pool>("ObstaclesHeadPool");
-        upPosition = GetNode<Node2D>("Up").Position;
-        height = GetNode<Node2D>("Height").Position.y;
-        xBoundary = GetNode<Node2D>("Boundary").Position.x;
-        rnd = new Random();
-    }
+        public float ObstaclesDelay { get => obstaclesDelay; set { obstaclesDelay = value; GetNode<Timer>("Timer").WaitTime = obstaclesDelay; } }
 
-    // Spawn 2 obstacles every obstaclesDelay
-    public void OnBuildTime() 
-    {
-        // Build up
-        var upObstacle = obstaclesPool.Spawn(upPosition, GetRandomScale()) as Obstacle;
-        upObstacle.Boundary = xBoundary;
-        var obstacleHead = obstaclesHeadPool.Spawn(new Vector2(upPosition.x, upObstacle.Height), Vector2.One * 2) as Obstacle;
-        float obstacleNormalHeight = upObstacle.NormalHeight;
-        // Build points    
-        pointsPool.Spawn(
-            new Vector2(upPosition.x, upObstacle.Height), 
-            new Vector2(2, spaceForPlayer / obstacleNormalHeight)
-        );
-        // Build down obstacle
-        float yUsed = upObstacle.Height + spaceForPlayer;
-        obstaclesHeadPool.Spawn(new Vector2(upPosition.x, yUsed - obstacleHead.Texture.GetHeight() * obstacleHead.Scale.y), Vector2.One * 2);
-        var downStartPos = new Vector2(upPosition.x, yUsed);
-        var downObstacle = obstaclesPool.Spawn(downStartPos, GetScaleLeft(obstacleNormalHeight, yUsed)) as Obstacle;
-        downObstacle.Boundary = xBoundary;
-    }
+        [Export]
+        private float obstaclesDelay = 4f;
+        [Export]
+        private int minObstacleHeight = 4;
+        [Export]
+        private int maxObstacleHeight = 9;
 
-    private Vector2 GetRandomScale() 
-    {
-        float randomY = rnd.Next(minObstacleHeight, maxObstacleHeight);
-        return new Vector2(2, randomY);
-    }
+        [Export]
+        private float spaceForPlayer = 100f;
 
-    private Vector2 GetScaleLeft(float obstacleNormalHeight, float yUsed) 
-    {
-        float yLeft = height - yUsed;
-        float scaleLeft = yLeft / obstacleNormalHeight;
-        return new Vector2(2, scaleLeft);
+        private Pool obstaclesPool;
+        private Pool pointsPool;
+        private Pool obstaclesHeadPool;
+
+        private Vector2 upPosition;
+        private float xBoundary;
+
+        private float height;
+        private Random rnd;
+
+        public override void _Ready()
+        {
+            obstaclesPool = GetNode<Pool>("ObstaclesPool");
+            pointsPool = GetNode<Pool>("PointsPool");
+            obstaclesHeadPool = GetNode<Pool>("ObstaclesHeadPool");
+            upPosition = GetNode<Node2D>("Up").Position;
+            height = GetNode<Node2D>("Height").Position.y;
+            xBoundary = GetNode<Node2D>("Boundary").Position.x;
+            rnd = new Random();
+        }
+
+        // Spawn 2 obstacles every obstaclesDelay
+        public void OnBuildTime()
+        {
+            // Build up
+            var upObstacle = obstaclesPool.Spawn(upPosition, GetRandomScale()) as Obstacle;
+            upObstacle.Boundary = xBoundary;
+            var obstacleHead = obstaclesHeadPool.Spawn(new Vector2(upPosition.x, upObstacle.Height), Vector2.One * 2) as Obstacle;
+            float obstacleNormalHeight = upObstacle.NormalHeight;
+            // Build points    
+            pointsPool.Spawn(
+                new Vector2(upPosition.x, upObstacle.Height),
+                new Vector2(2, spaceForPlayer / obstacleNormalHeight)
+            );
+            // Build down obstacle
+            float yUsed = upObstacle.Height + spaceForPlayer;
+            obstaclesHeadPool.Spawn(new Vector2(upPosition.x, yUsed - obstacleHead.Texture.GetHeight() * obstacleHead.Scale.y), Vector2.One * 2);
+            var downStartPos = new Vector2(upPosition.x, yUsed);
+            var downObstacle = obstaclesPool.Spawn(downStartPos, GetScaleLeft(obstacleNormalHeight, yUsed)) as Obstacle;
+            downObstacle.Boundary = xBoundary;
+        }
+
+        private Vector2 GetRandomScale()
+        {
+            float randomY = rnd.Next(minObstacleHeight, maxObstacleHeight);
+            return new Vector2(2, randomY);
+        }
+
+        private Vector2 GetScaleLeft(float obstacleNormalHeight, float yUsed)
+        {
+            float yLeft = height - yUsed;
+            float scaleLeft = yLeft / obstacleNormalHeight;
+            return new Vector2(2, scaleLeft);
+        }
     }
 }
